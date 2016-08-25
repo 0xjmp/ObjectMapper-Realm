e4ea1508ed594a30c1a4a5e9ab867240f0c6e985
jakenberg <jsksma2@gmail.com>
2016-08-25 12:55:33 -0700

    Add umbrella header

--- ObjectMapper+Realm.podspec
+++ ObjectMapper+Realm.podspec
@@ -10,7 +10,6 @@ Pod::Spec.new do |s|
 
   s.ios.deployment_target = "8.0"
   s.osx.deployment_target = "10.9"
-  s.tvos.deployment_target = "9.0"
 
   s.requires_arc = "true"
   s.source_files = "ObjectMapper+Realm/**/*.swift"
---
+++ ObjectMapper+Realm/ObjectMapper_Realm.h
@@ -0,0 +1,19 @@
+//
+//  ObjectMapper+Realm.h
+//  ObjectMapper+Realm
+//
+//  Created by Jake Peterson on 8/25/16.
+//  Copyright © 2016 jakenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjectMapper+Realm.
+FOUNDATION_EXPORT double ObjectMapper_RealmVersionNumber;
+
+//! Project version string for ObjectMapper+Realm.
+FOUNDATION_EXPORT const unsigned char ObjectMapper_RealmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectMapper_Realm/PublicHeader.h>
+
+
